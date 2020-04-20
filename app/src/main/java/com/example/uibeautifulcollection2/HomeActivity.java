8e3b81b00e251747714001dc14717e9bb2457381
JeffKi11er <45094606+JeffKi11er@users.noreply.github.com>
2020-04-20 09:42:53 +0700

    [Java file] Update home screen

--- app/src/main/java/com/example/uibeautifulcollection2/HomeActivity.java
+++ app/src/main/java/com/example/uibeautifulcollection2/HomeActivity.java
@@ -1,14 +1,25 @@
 package com.example.uibeautifulcollection2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class HomeActivity extends AppCompatActivity {
-
+    private DrawerLayout drawerLayout;
+    private NavigationView navView;
+    private FrameLayout container;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        init();
+    }
+
+    private void init() {
     }
 }
