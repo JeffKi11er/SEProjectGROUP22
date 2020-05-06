bb997ec54038a903afc54deb53a20a21b8026976
JeffKi11er <45094606+JeffKi11er@users.noreply.github.com>
2020-05-06 11:17:56 +0700

    Update home screen, fixes #17

--- app/src/main/java/com/example/uibeautifulcollection2/HomeActivity.java
+++ app/src/main/java/com/example/uibeautifulcollection2/HomeActivity.java
@@ -32,40 +32,40 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
     private ImageView imgButtonBar;
     private ImageView imgButtonSearch;
     private BottomNavigationView bottomNavigationView;
-    private View headerView;
-    private TextView tvNameHeader;
-    private ImageView imgHeader;
+//    private View headerView;
+//    private TextView tvNameHeader;
+//    private ImageView imgHeader;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         init();
-        loadInformation();
+        //loadInformation();
     }
 
-    private void loadInformation() {
-        FirebaseUser user = firebaseAuth.getCurrentUser();
-        if(user!=null){
-            if(user.getDisplayName()!=null){
-                tvNameHeader.setText(user.getDisplayName());
-            }
-            if(user.getPhotoUrl()!=null){
-                Glide.with(this).load(user.getPhotoUrl().toString()).into(imgHeader);
-            }
-        }
-    }
+//    private void loadInformation() {
+//        FirebaseUser user = firebaseAuth.getCurrentUser();
+//        if(user!=null){
+//            if(user.getDisplayName()!=null){
+//                tvNameHeader.setText(user.getDisplayName());
+//            }
+//            if(user.getPhotoUrl()!=null){
+//                Glide.with(this).load(user.getPhotoUrl().toString()).into(imgHeader);
+//            }
+//        }
+//    }
 
     private void init() {
         firebaseAuth = FirebaseAuth.getInstance();
         imgButtonBar = (ImageView)findViewById(R.id.img_buttons);
         drawerLayout = (DrawerLayout)findViewById(R.id.drawer);
-        navView = (NavigationView)findViewById(R.id.navigation_view);
-        headerView = navView.getHeaderView(0);
-        tvNameHeader = (TextView)headerView.findViewById(R.id.header_name);
-        imgHeader = (ImageView)headerView.findViewById(R.id.header_face);
+        //navView = (NavigationView)findViewById(R.id.navigation_view);
+        //headerView = navView.getHeaderView(0);
+        //tvNameHeader = (TextView)headerView.findViewById(R.id.header_name);
+        //imgHeader = (ImageView)headerView.findViewById(R.id.header_face);
         imgButtonSearch = (ImageView)findViewById(R.id.img_btn_search);
-        tvNameHeader.setText("Thiet");
-        imgHeader.setImageResource(R.drawable.cat_face);
+       // tvNameHeader.setText("Thiet");
+       // imgHeader.setImageResource(R.drawable.cat_face);
         bottomNavigationView = (BottomNavigationView)findViewById(R.id.bottom_nav);
         bottomNavigationView.setOnNavigationItemSelectedListener(botListener);
         imgButtonBar.setOnClickListener(this);
